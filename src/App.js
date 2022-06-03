399a2041dbfc4860b4fc8c39b2f95b69443b87e6
unknown <skj99667788@gmail.com>
2022-06-03 09:54:57 -0700

    init components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Categories from './pages/Categories';
+import NotFound from './pages/NotFound';
+import NavBar from './componenets/NavBar/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" exact="true" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
